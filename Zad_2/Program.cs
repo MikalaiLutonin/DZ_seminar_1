057a9902e84e1888f35aa362b3f7f71f7922b03a
Mikalai Lutonin <m.lutonin@gmail.com>
2022-09-16 21:29:57 +0300

    Написали программу задачи 2 ДЗ семинара 1

---
+++ Zad_2/Program.cs
@@ -0,0 +1,29 @@
+﻿/* Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+a = 5; b = 7 -> max = 7
+a = 2 b = 10 -> max = 10
+a = -9 b = -3 -> max = -3
+
+*/
+
+
+Console.WriteLine("Введите первое число: ");
+int n1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int n2= int.Parse(Console.ReadLine());
+if (n1>n2)
+{
+    Console.Write("Максимальное число: ");
+    Console.WriteLine(n1);
+}
+else
+{
+    if (n1<n2)
+    {
+    Console.Write("Максимальное число: ");
+    Console.WriteLine(n2);}
+    else 
+    {
+    Console.Write("Оба числа равны");
+    }
+}
